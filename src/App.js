1bc04f49c4cd11febb2cf6b6e3c79a1f2592a7ed
Marvin Berrio <marvinccp@gmail.com>
2023-01-26 01:19:55 +0100

    Create a Woman component and insert video

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
+import Woman from './components/Woman'
 import './styles/App.css'
 
 const App = () => {
   return (
     <>
     <Header />
+    <Woman />
     </>
   )
 }
