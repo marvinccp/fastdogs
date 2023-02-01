9e09a2644a97db50b65dcd9dbb274261b7d6455e
Marvin Berrio <marvinccp@gmail.com>
2023-02-02 00:22:59 +0100

    Burguers page data fetching

--- src/components/Burguers.jsx
+++ src/components/Burguers.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+//components
+import Header from "./Header";
+
+//styles
+import "../styles/Burguers.css";
+
+//data
+import burguers from "../data/burgers";
 
 const Burguers = () => {
   return (
-    <div>
-        <Header />
+    <>
+      <Header />
+      <title className="title-container">
         <h1>Burguers</h1>
-    </div>
-  )
-}
+      </title>
+      <section className="items-cards-container">
+        {burguers.map((item) => (
+          <article className="card-item-container" key={item.id}>
+            <h2>{item.title}</h2>
+            <img src={item.thumb} alt={item.title} />
+            <h3>Price $ {item.price}</h3>
+          </article>
+        ))}
+      </section>
+    </>
+  );
+};
 
-export default Burguers
+export default Burguers;
---
+++ src/data/burgers.js
@@ -0,0 +1,34 @@
+const burguers = [
+  {
+    id: 1,
+    title: "Burguer Max",
+    thumb: "./assets/data_images/burguer.png",
+    price: 8,
+  },
+  {
+    id: 2,
+    title: "Burguer Passion",
+    thumb: "./assets/data_images/burguer.png",
+    price: 10,
+  },
+  {
+    id: 3,
+    title: "Tex Mex Burguer",
+    thumb: "./assets/data_images/burguer.png",
+    price: 12,
+  },
+  {
+    id: 4,
+    title: "Space Burguer",
+    thumb: "./assets/data_images/burguer.png",
+    price: 11,
+  },
+  {
+    id: 5,
+    title: "Mega Cheese Burguer",
+    thumb: "./assets/data_images/burguer.png",
+    price: 18
+  },
+];
+
+export default burguers;
